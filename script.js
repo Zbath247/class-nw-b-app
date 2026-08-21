fdeca5988dc95bb20f0f3cb3a22c0a9a5316a145
Zbath247 <moksambath329@gmail.com>
2026-08-21 16:13:19 +0700

    Update and rename script.js.txt to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GOOGLE_SHEET_API_URL = "https://script.google.com/macros/s/AKfycby0QuL4j2H-avVtgvbjtddPTnesxv3T0VJwMwDGGmAiS-7pHzJpG4MTa0gAgXogZTDR/exec";
+const GOOGLE_SHEET_API_URL = "https://script.google.com/macros/s/AKfycbz2f3cZh5hqwqvIt3QcYtE9CycoBpC9AiBv3h3CfHPkZm3UhLGfrwIbEecDW5axluGu/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
   const tableBody = document.getElementById("schedule-body");
@@ -8,8 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
   tableBody.innerHTML = `<tr><td colspan="5" style="text-align:center;">កំពុងទាញយកទិន្នន័យពី Google Sheets...</td></tr>`;
 
   // ទាញទិន្នន័យពី Google Sheets API
-  fetch(GOOGLE_SHEET_API_URL)
-    .then(response => response.json())
+  fetch(GOOGLE_SHEET_API_URL, { redirect: "follow" })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
     .then(schedules => {
       // លុបទិន្នន័យចាស់ក្នុងតារាង
       tableBody.innerHTML = "";
